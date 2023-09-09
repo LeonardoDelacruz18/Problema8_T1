b4171d43cf659bd0b9b20f857c83593a6f6c9eb3
LeonardoDelacruz18 <142689646+LeonardoDelacruz18@users.noreply.github.com>
2023-09-09 12:40:50 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/problema8t1/Problema8T1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.problema8t1;
+
+/**
+ *
+ * @author USER
+ */
+public class Problema8T1 {
+
+    public static void main(String[] args) {
+        AgregarPersona ventana = new  AgregarPersona();
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+    }
+}
